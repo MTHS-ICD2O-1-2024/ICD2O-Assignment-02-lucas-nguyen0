41912eaef868f3ce6e4dd15dc7fdd6feba1e426b
default <lucas.nguyen@mths.ca>
2025-03-19 16:42:28 +0000

    added units

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Lucas Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lucas Nguyen
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
-/**
- * Displays an alert with the message "Hello, World!"
- */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const length = document.getElementById("length").value
+  const width = document.getElementById("width").value
+  const height = document.getElementById("height").value
+  // output
+  document.getElementById("answer").innerHTML =
+    "The surface area is: " + (2 * ((width * length) + (height * length) + (width * height))) + "cm²."
 }
